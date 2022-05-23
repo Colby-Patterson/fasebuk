de308b08d1a6090d84c9e72e9b1d27aeeaf6599a
Colby Patterson <cpatt@byui.edu>
2022-05-23 15:27:27 -0600

    Adding styling

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import './App.css';
 import Post from "./Post";
 
 let users = [
-  {id:1, name:"Bob", post:"This is test post 1"},
-  {id:2, name:"Joe", post:"This is test post 2"},
-  {id:3, name:"George", post:"This is test post 3"},
+  {id:1, name:"Bob", post:"This is test post 1", initialLikes: 0},
+  {id:2, name:"Joe", post:"This is test post 2", initialLikes: 0},
+  {id:3, name:"George", post:"This is test post 3", initialLikes: 0},
+  {id:4, name:"Jane", post:"This is test post 4", initialLikes: 0},
+  {id:5, name:"Joanne", post:"This is test post 5", initialLikes: 0},
+  {id:6, name:"Eric", post:"This is test post 6", initialLikes: 0},
+  {id:7, name:"Debra", post:"This is test post 7", initialLikes: 0},
+  {id:8, name:"Scott", post:"This is test post 8", initialLikes: 0},
 ]
 
 function App() {
   const renderPosts = ()=>{
     return users.map((user)=>{
-      return <Post key={user.id} name={user.name} post={user.post} />
+      return <Post key={user.id} name={user.name} post={user.post} likes={user.initialLikes} />
     })
 }
   
--- src/Post.js
+++ src/Post.js
@@ -1,11 +1,17 @@
+import React, { useState } from "react";
 const Post = (props) => {
-  
+  let [likes, setLikes] = useState(props.initialLikes || 0);
+  const like = () => {
+    setLikes(likes + 1)
+  };
+
   return (
     <div className="container">
       <div>
-        <h3>User Name</h3>
-        <p>{props.name}</p>
-        <p>{props.post}</p>
+        <strong><p>{props.name}</p></strong>
+        <i><p>{props.post}</p></i>
+        <p>Likes: {likes}</p>
+        <button onClick={like}>Like</button>
       </div>
     </div>  
   )
