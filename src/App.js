59607c64cbaa588035b3cd5255353f7800df0abf
Colby Patterson <cpatt@byui.edu>
2022-05-23 19:51:24 -0600

    Show comments working

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import './App.css';
 import Post from "./Post";
 
 let users = [
-  {id:1, name:"Bob", post:"Loving the weather today!", initialLikes: 0, comments: ['This is comment 1', 'This is comment 2']},
-  {id:2, name:"Joe", post:"This is a much longer test post than the others.", initialLikes: 0},
-  {id:3, name:"George", post:"Just joined. Who else is on here?", initialLikes: 0},
-  {id:4, name:"Jane", post:"Utah Jazz FTW!", initialLikes: 0},
-  {id:5, name:"Joanne", post:"Just bought a new car", initialLikes: 0},
-  {id:6, name:"Eric", post:"Starting my new job today. Wish me luck!", initialLikes: 0},
-  {id:7, name:"Debra", post:"What's everyone doing for Memorial Day weekend?", initialLikes: 0},
-  {id:8, name:"Scott", post:"Any advice on where to find good mangoes?", initialLikes: 0},
-  {id:9, name:"Carl", post:"This is another test post", initialLikes: 0},
-  {id:10, name:"Daniele", post:"This is another test post", initialLikes: 0},
-  {id:11, name:"Arnold", post:"This is another test post", initialLikes: 0},
-  {id:12, name:"Anne", post:"This is another test post", initialLikes: 0},
+  {id:1, name:"Bob", post:"Loving the weather today!", initialLikes: 0, comments: ['This is comment 1', 'This is comment 2', 'This is comment 3']},
+  {id:2, name:"Joe", post:"This is a much longer test post than the others.", initialLikes: 0, comments: []},
+  {id:3, name:"George", post:"Just joined. Who else is on here?", initialLikes: 0, comments: []},
+  {id:4, name:"Jane", post:"Utah Jazz FTW!", initialLikes: 0, comments: []},
+  {id:5, name:"Joanne", post:"Just bought a new car", initialLikes: 0, comments: []},
+  {id:6, name:"Eric", post:"Starting my new job today. Wish me luck!", initialLikes: 0, comments: []},
+  {id:7, name:"Debra", post:"What's everyone doing for Memorial Day weekend?", initialLikes: 0, comments: []},
+  {id:8, name:"Scott", post:"Any advice on where to find good mangoes?", initialLikes: 0, comments: []},
+  {id:9, name:"Carl", post:"This is another test post", initialLikes: 0, comments: []},
+  {id:10, name:"Daniele", post:"This is another test post", initialLikes: 0, comments: []},
+  {id:11, name:"Arnold", post:"This is another test post", initialLikes: 0, comments: []},
+  {id:12, name:"Anne", post:"This is another test post", initialLikes: 0, comments: []}
 ]
 
 function App() {
--- src/Post.js
+++ src/Post.js
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 const Post = (props) => {
   let [showComments, setShowComments] = useState(false);
   let [likes, setLikes] = useState(props.initialLikes || 0);
+  const listComments = (comments)=>{
+    return comments.map((comment)=> <li>{comment}</li>
+    )
+  }
   const like = () => {
     setLikes(likes + 1)
   };
@@ -23,11 +27,7 @@ const Post = (props) => {
           >
             {showComments ? 'Hide Comments':'Show Comments'}
             </button>
-            {showComments && 
-            <ul>
-              <li>{props.comments[0]}</li>
-              <li>{props.comments[1]}</li>
-            </ul>}
+            {showComments && listComments(props.comments)}
         {/* </div> */}
       </div>
     </div>  
